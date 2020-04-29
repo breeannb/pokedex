5b9498573f917a0c438c927b94a236a1f895b069
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-29 16:15:01 -0700

    state is successfully working for and added travis

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 
 export default class App extends Component {
   
-  handleChange = () => {
-    console.log('hello world');
+  // initialize state 
+  state = {
+    searchQuery: null, 
+  }
+
+   
+  handleChange = (event) => {
+      // console.log('hello world'); successfully logs out
+    //get value of the input 
+    const value = event.target.value; 
+    // on change, we can now get the value
+      // console.log('hello world!,', value); successfully logs out
+    // set state of query to value
+    this.setState({ searchQuery: value});
+    
   }
 
   render() {
+      // console.log(this.state.searchQuery); successfully logs out our state changes upon onChange
     return (
       <div className="App">
         

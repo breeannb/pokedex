83183789b1d51d8d7dbc6f91d2e76b1cdb45db54
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-29 18:10:54 -0700

    getting rid of some external things in test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import App from './App';
 import { shallow } from 'enzyme'; 
+import Header from './Header';
 
 test('renders learn react link', () => {
   it('renders correctly', () => {
-    const wrapper = shallow(<MyComponent />)
+    const wrapper = shallow(<Header />)
    expect(wrapper).toMatchSnapshot();
 });
 });

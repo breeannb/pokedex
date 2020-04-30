bb6afb6ac6a3fe897e11a5b0f9a5c28337ddeb2e
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-30 16:47:35 -0700

    I have a few search bars and handlers that are working including input so saving here

--- src/App.js
+++ src/App.js
@@ -25,29 +25,72 @@ export default class App extends Component {
   
   // initialize state 
   state = {
-    searchQuery: '', 
+    searchNameQuery: '', 
+    searchTypeQuery: '',
+    searchAttackQuery: '',
     data: data, 
   }
 
-   
-  handleChange = (event) => {
+  handleNameChange = (event) => {
       // console.log('hello world'); successfully logs out
     //get value of the input 
     const value = event.target.value; 
     // on change, we can now get the value
       // console.log('hello world!,', value); successfully logs out
     // set state of query to value
-    this.setState({ searchQuery: value});
+    this.setState({ searchNameQuery: value});
+    this.setState({ searchTypeQuery: value});
     
   }
 
-  handleClick = async () => { 
+  handleTypeChange = (event) => {
+    // console.log('hello world'); successfully logs out
+  //get value of the input 
+  const value = event.target.value; 
+  // on change, we can now get the value
+    // console.log('hello world!,', value); successfully logs out
+  // set state of query to value
+  this.setState({ searchNameQuery: value});
+  this.setState({ searchTypeQuery: value});
+  
+  }
+
+  handleAttackChange = (event) => {
+      // console.log('hello world'); successfully logs out
+    //get value of the input 
+    const value = event.target.value; 
+    // on change, we can now get the value
+      // console.log('hello world!,', value); successfully logs out
+    // set state of query to value
+    this.setState({ searchAttackQuery: value});
+    
+  }
+
+  handleNameClick = async () => { 
     // console.log('hello world', this.state.searchQuery);
-    const fetchData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.searchQuery}`); 
+    const fetchData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.searchNameQuery}`); 
 
     console.log(fetchData.body.results);
     this.setState({ data: fetchData.body.results })
   }
+
+  handleTypeClick = async () => { 
+    // console.log('hello world', this.state.searchQuery);
+    const fetchData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?type_1=${this.state.searchTypeQuery}`); 
+
+    console.log(fetchData.body.results);
+    this.setState({ data: fetchData.body.results })
+  }
+
+  handleAttackClick = async () => { 
+    // console.log('hello world', this.state.searchQuery);
+    const fetchData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?attack=${this.state.searchAttackQuery}`); 
+
+    console.log(fetchData.body.results);
+    this.setState({ data: fetchData.body.results })
+  }
+
+
 
   render() {
       // console.log(this.state.searchQuery); successfully logs out our state changes upon onChange
@@ -58,11 +101,21 @@ export default class App extends Component {
         <Header />
         <div className="App">
           
+          {/* search bar by name  */}
           {/* make a search bar event handler*/}
-          <input onChange={this.handleChange} />
+          <input onChange={this.handleNameChange} />
+          <button onClick={this.handleNameClick}>Name Search</button>
 
-          {/* button */}
-          <button onClick={this.handleClick}>Search</button>
+          {/* search bar by types: there can be 2 */}
+          {/* make a search bar event handler*/}
+          <input onChange={this.handleTypeChange} />
+          <button onClick={this.handleTypeClick}>Type Search</button>
+
+          {/* search bar by types: there can be 2 */}
+          {/* make a search bar event handler*/}
+          <input onChange={this.handleAttackChange} type="number" min="1" max="100"/>
+          <button onClick={this.handleAttackClick}>Attack Search</button>
+
 
           {/* Map over the data */}
           {

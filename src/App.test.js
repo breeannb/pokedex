10927ec826056512922709d323c1fc89dcadd48b
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-05-04 01:14:38 -0700

    tested my third component and passed

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme'; 
 import Header from './Header';
 import Footer from './Footer';
+import SearchSection from './SearchSection';
+
 // import { waitForElementToBeRemoved } from '@testing-library/react';
 
 test('render a header', () => {
@@ -14,4 +16,10 @@ test('render a footer', () => {
   const footer = shallow(<Footer />)
 //  expect(header).toMatchSnapshot();
 expect(footer.find('p').length).toBe(1);
+});
+
+test('render a pokecard', () => {
+  const pokecard = shallow(<SearchSection />)
+//  expect(header).toMatchSnapshot();
+expect(pokecard.find('button').length).toBe(5);
 });

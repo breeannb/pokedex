5d2760ef4b93ee7cee911a45d1693d14f59e4a9c
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-05-04 01:01:26 -0700

    Updates

---
+++ precache-manifest.fd853836ebbad56b5e0de6b8f141c472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f45999f83124317e58b5ffc29726bb",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "b00eac22fd02e2fae807",
+    "url": "/pokedex/static/css/main.b72e6a69.chunk.css"
+  },
+  {
+    "revision": "2495bb092228333102bf",
+    "url": "/pokedex/static/js/2.42ba9e42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.42ba9e42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00eac22fd02e2fae807",
+    "url": "/pokedex/static/js/main.fa16e150.chunk.js"
+  },
+  {
+    "revision": "d367ed54df4579fc06b7",
+    "url": "/pokedex/static/js/runtime-main.a8c1be9c.js"
+  }
+]);

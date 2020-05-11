fdecdb96e6e50daf9d6a24bb58366a580d8d4bc1
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-05-10 21:38:19 -0700

    got one test to work, had some packagejson issues, but seems to be running now saving just in case

---
+++ src/test-files/SearchBar.test.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import { shallow } from 'enzyme'; 
+import SearchBar from '../SearchBar.js';
+
+it('renders a searchbar', () => {
+  const wrapper = shallow(<SearchBar />);
+
+  expect(wrapper).toMatchSnapshot();
+
+}); 

6db6207d1d367d1d03e499b915888a8bba177db5
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-05-10 21:54:04 -0700

    finally got 2nd snapshot to pass

--- src/SearchBar.test.js
+++ src/SearchBar.test.js
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import { shallow } from 'enzyme'; 
-import SearchBar from '../SearchBar.js';
+import SearchBar from './SearchBar.js';
 
 it('renders a searchbar', () => {
   const wrapper = shallow(<SearchBar />);
